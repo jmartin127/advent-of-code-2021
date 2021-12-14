d511842c984959a3029fcc27a0de607ac7bdaea3
Jeff Martin <jmartin127@gmail.com>
2021-12-14 00:55:10 -0700

    Day 14 - Cleaning up code

--- day14/part2/main.go
+++ day14/part2/main.go
@@ -8,14 +8,13 @@ import (
 	"github.com/jmartin127/advent-of-code-2021/helpers"
 )
 
-var inputsAlreadyCalculated map[string]map[rune]int
+var inputsAlreadyCalculated = make(map[string]map[rune]int, 0)
+
+const NUM_ITERATIONS = 40
 
 func main() {
 	filepath := "input.txt"
 	list := helpers.ReadFile(filepath)
-
-	inputsAlreadyCalculated = make(map[string]map[rune]int, 0)
-
 	insertions := make(map[string]rune, 0)
 	for i := 2; i < len(list); i++ {
 		k, v := readInsertionLine(list[i])
@@ -23,16 +22,14 @@ func main() {
 	}
 
 	polymer := list[0]
-	numIterationsToDo := 10
-	countByChar := make(map[rune]int, 0)
-	findCountsAfterNIterations([]rune(polymer), insertions, numIterationsToDo, countByChar)
+	result := findCountsAfterNIterations([]rune(polymer), insertions, NUM_ITERATIONS)
 
 	// add the last char
 	chars := []rune(polymer)
 	lastChar := chars[len(chars)-1]
-	countByChar[lastChar] = countByChar[lastChar] + 1
+	result[lastChar] = result[lastChar] + 1
 
-	top, bottom := countMostLeast(countByChar)
+	top, bottom := countMostLeast(result)
 	fmt.Printf("Top bottom %d %d\n", top, bottom)
 	fmt.Printf("Answer %d\n", top-bottom)
 }
@@ -56,27 +53,36 @@ func countMostLeast(counts map[rune]int) (int, int) {
 	return top, bottom
 }
 
-func findCountsAfterNIterations(input []rune, insertions map[string]rune, iterationsRemaining int, countByChar map[rune]int) {
-	inputStr := string(input)
-	mapKey := fmt.Sprintf("%s%d", inputStr, iterationsRemaining)
-	if val, ok := inputsAlreadyCalculated[mapKey]; ok {
-		addPrecomputedResultToCounts(val, countByChar)
-		return
+func findCountsAfterNIterations(input []rune, insertions map[string]rune, iterationsRemaining int) map[rune]int {
+	// see if we already know the answer
+	mapKey := fmt.Sprintf("%s%d", string(input), iterationsRemaining)
+	if precomupted, ok := inputsAlreadyCalculated[mapKey]; ok {
+		return precomupted
 	}
 
 	// add to the counts
 	if iterationsRemaining == 0 {
-		addToCounts(input, countByChar, iterationsRemaining)
-		return
+		return computeCounts(input)
 	}
 
+	// since we don't know the answer, compute it recursively
+	total := make(map[rune]int, 0)
 	for i := 0; i < len(input)-1; i++ {
 		first := input[i]
 		second := input[i+1]
 		insertion := insertions[string(first)+string(second)]
 
-		// count first pair
-		findCountsAfterNIterations([]rune{first, insertion, second}, insertions, iterationsRemaining-1, countByChar)
+		result := findCountsAfterNIterations([]rune{first, insertion, second}, insertions, iterationsRemaining-1)
+		addToTotal(total, result)
+	}
+
+	cachePrecomputed(mapKey, total)
+	return total
+}
+
+func addToTotal(total map[rune]int, toAdd map[rune]int) {
+	for k, v := range toAdd {
+		total[k] = total[k] + v
 	}
 }
 
@@ -86,21 +92,20 @@ func addPrecomputedResultToCounts(precomupted map[rune]int, countByChar map[rune
 	}
 }
 
-func cachePrecomputed(input []rune, iterationsRemaining int) map[rune]int {
+func cachePrecomputed(mapKey string, precomputed map[rune]int) {
+	inputsAlreadyCalculated[mapKey] = precomputed
+}
+
+func computeCounts(input []rune) map[rune]int {
 	precomupted := make(map[rune]int, 0)
 	for i := 0; i < len(input)-1; i++ {
 		precomupted[input[i]] = precomupted[input[i]] + 1
 	}
 
-	mapKey := fmt.Sprintf("%s%d", string(input), iterationsRemaining)
-	inputsAlreadyCalculated[mapKey] = precomupted
+	mapKey := fmt.Sprintf("%s%d", string(input), 0) // only on zeroth iteration
+	cachePrecomputed(mapKey, precomupted)
 
 	return precomupted
-}
-
-func addToCounts(input []rune, countByChar map[rune]int, iterationsRemaining int) {
-	precomupted := cachePrecomputed(input, iterationsRemaining)
-	addPrecomputedResultToCounts(precomupted, countByChar)
 }
 
 func readInsertionLine(line string) (string, rune) {
