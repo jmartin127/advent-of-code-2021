6e3ecc84ef40ed0518508da342dc254b13fa9258
Jeff Martin <jmartin127@gmail.com>
2021-12-14 00:55:34 -0700

    Day 14 - Cleaning up code #2

--- day14/part2/main.go
+++ day14/part2/main.go
@@ -8,51 +8,45 @@ import (
 	"github.com/jmartin127/advent-of-code-2021/helpers"
 )
 
-var inputsAlreadyCalculated = make(map[string]map[rune]int, 0)
-
 const NUM_ITERATIONS = 40
 
+var inputsAlreadyCalculated = make(map[string]map[rune]int, 0)
+
 func main() {
-	filepath := "input.txt"
-	list := helpers.ReadFile(filepath)
+	// parse the input
+	list := helpers.ReadFile("input.txt")
 	insertions := make(map[string]rune, 0)
 	for i := 2; i < len(list); i++ {
 		k, v := readInsertionLine(list[i])
 		insertions[k] = v
 	}
 
+	// iterate
 	polymer := list[0]
 	result := findCountsAfterNIterations([]rune(polymer), insertions, NUM_ITERATIONS)
 
-	// add the last char
+	// add a count for the last character
 	chars := []rune(polymer)
 	lastChar := chars[len(chars)-1]
 	result[lastChar] = result[lastChar] + 1
 
+	// compute the answer
 	top, bottom := countMostLeast(result)
 	fmt.Printf("Top bottom %d %d\n", top, bottom)
 	fmt.Printf("Answer %d\n", top-bottom)
 }
 
-func printMap(counts map[string]int) {
-	for k, v := range counts {
-		fmt.Printf("k=%s,v=%d\n", k, v)
-	}
-}
-
 func countMostLeast(counts map[rune]int) (int, int) {
-	// get the top values
 	values := make([]int, 0)
 	for _, v := range counts {
 		values = append(values, v)
 	}
 	sort.Sort(sort.Reverse(sort.IntSlice(values)))
-
-	top := values[0]
-	bottom := values[len(values)-1]
-	return top, bottom
+	return values[0], values[len(values)-1]
 }
 
+// main premise is that we can recurively determine the character counts for each subset of the problem
+// also, since the numbers are so huge, don't recompute something we've already computed
 func findCountsAfterNIterations(input []rune, insertions map[string]rune, iterationsRemaining int) map[rune]int {
 	// see if we already know the answer
 	mapKey := fmt.Sprintf("%s%d", string(input), iterationsRemaining)
@@ -76,7 +70,7 @@ func findCountsAfterNIterations(input []rune, insertions map[string]rune, iterat
 		addToTotal(total, result)
 	}
 
-	cachePrecomputed(mapKey, total)
+	inputsAlreadyCalculated[mapKey] = total
 	return total
 }
 
@@ -86,31 +80,17 @@ func addToTotal(total map[rune]int, toAdd map[rune]int) {
 	}
 }
 
-func addPrecomputedResultToCounts(precomupted map[rune]int, countByChar map[rune]int) {
-	for k, v := range precomupted {
-		countByChar[k] = countByChar[k] + v
-	}
-}
-
-func cachePrecomputed(mapKey string, precomputed map[rune]int) {
-	inputsAlreadyCalculated[mapKey] = precomputed
-}
-
 func computeCounts(input []rune) map[rune]int {
 	precomupted := make(map[rune]int, 0)
 	for i := 0; i < len(input)-1; i++ {
 		precomupted[input[i]] = precomupted[input[i]] + 1
 	}
-
 	mapKey := fmt.Sprintf("%s%d", string(input), 0) // only on zeroth iteration
-	cachePrecomputed(mapKey, precomupted)
-
+	inputsAlreadyCalculated[mapKey] = precomupted
 	return precomupted
 }
 
 func readInsertionLine(line string) (string, rune) {
 	parts := strings.Split(line, " -> ") // CH -> B
-	key := parts[0]
-	val := parts[1]
-	return key, []rune(val)[0]
+	return parts[0], []rune(parts[1])[0]
 }
