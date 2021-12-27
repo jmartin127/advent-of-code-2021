d193041ce60fdcb8b5e82faa87ebf41ed0a02b9f
Jeff Martin <jmartin127@gmail.com>
2021-12-27 14:23:51 -0700

    Day 24: Part 1 - Input is being parsed

---
+++ day24/part1/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/jmartin127/advent-of-code-2021/helpers"
+)
+
+type instruction struct {
+	cmd              string
+	operand1         string
+	hasSecondOperand bool
+	operand2IsNum    bool
+	operand2         string
+	operand2Num      int
+}
+
+func main() {
+	filepath := "day24/input.txt"
+	list := helpers.ReadFile(filepath)
+	instructions := parseInput(list)
+
+	for _, i := range instructions {
+		fmt.Printf("i=%+v\n", i)
+	}
+}
+
+// func checkModelNumber(modelNumStr int) {
+// 	modelNum := strings.Split(strconv.Itoa(modelNumStr), "")
+// 	var modelNumPointer int
+
+// }
+
+func parseInput(list []string) []*instruction {
+	instructions := make([]*instruction, 0)
+	for _, line := range list {
+		instructions = append(instructions, parseLine(line))
+	}
+	return instructions
+}
+
+/*
+inp w
+mul x 0
+add x z
+mod x 26
+div z 1
+add x 13
+eql x w
+eql x 0
+
+inp a - Read an input value and write it to variable a.
+add a b - Add the value of a to the value of b, then store the result in variable a.
+mul a b - Multiply the value of a by the value of b, then store the result in variable a.
+div a b - Divide the value of a by the value of b, truncate the result to an integer, then store the result in variable a. (Here, "truncate" means to round the value toward zero.)
+mod a b - Divide the value of a by the value of b, then store the remainder in variable a. (This is also called the modulo operation.)
+eql a b - If the value of a and b are equal, then store the value 1 in variable a. Otherwise, store the value 0 in variable a.
+*/
+func parseLine(line string) *instruction {
+	parts := strings.Split(line, " ")
+
+	var hasSecondOperand bool
+	var operand2 string
+	var operand2Num int
+	var operand2IsNum bool
+	if len(parts) > 2 {
+		hasSecondOperand = true
+		operand2Str := parts[2]
+		if operand2Str == "w" || operand2Str == "x" || operand2Str == "y" || operand2Str == "z" {
+			operand2 = operand2Str
+		} else {
+			operand2IsNum = true
+			v, _ := strconv.Atoi(operand2Str)
+			operand2Num = v
+		}
+	}
+
+	return &instruction{
+		cmd:              parts[0],
+		operand1:         parts[1],
+		hasSecondOperand: hasSecondOperand,
+		operand2:         operand2,
+		operand2Num:      operand2Num,
+		operand2IsNum:    operand2IsNum,
+	}
+}
